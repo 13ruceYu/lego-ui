bb36589c1706bf4f4a7e590285570758a6aeff81
13ruceYu <bruceyuhongbo@gmail.com>
2023-04-13 12:56:11 +0800

    chore: add tailwind.css

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 
+import './styles/index.css'
 import 'ant-design-vue/dist/antd.css'
 
 const app = createApp(App)
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -2,7 +2,9 @@
 
 <template>
   <a-row>
-    <a-col :span="12">hello</a-col>
+    <a-col :span="12">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </a-col>
     <a-col :span="12">hello</a-col>
   </a-row>
 </template>
