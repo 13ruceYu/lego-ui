beb55ced0921656e6d2092d4479fe73826e2d7d8
13ruceYu <bruceyuhongbo@gmail.com>
2023-05-08 22:55:10 +0800

    feat: init editor store

---
+++ src/store/editor/editor.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+import { v4 as uuidv4 } from 'uuid'
+
+interface ComponentData {
+  props: { [key: string]: any }
+  id: string
+  name: string
+}
+
+export interface EditorProps {
+  // 供中间编辑器渲染的数组
+  components: ComponentData[]
+  // 当前编辑的元素
+  currentElement: string
+  // 其余信息之后补充
+}
+
+export const testComponents: ComponentData[] = [
+  { id: uuidv4(), name: 'l-text', props: { text: 'hello' } },
+  { id: uuidv4(), name: 'l-text', props: { text: 'world' } }
+]
+
+export const useEditorStore = defineStore({
+  id: 'editor',
+  state: (): EditorProps => ({
+    components: testComponents,
+    currentElement: ''
+  })
+})
--- src/views/editor/Editor.vue
+++ src/views/editor/Editor.vue
@@ -1,9 +1,22 @@
+<script lang="ts" setup>
+import { useEditorStore } from '@/store/editor/editor'
+
+const editorStore = useEditorStore()
+</script>
+
 <template>
-  <div class="editor flex justify-between h-screen border-2 border-blue-400">
+  <div
+    class="editor [&>*]:m-2 flex justify-between h-screen border-2 border-blue-400"
+  >
     <div class="component-list w-60 border-2 border-yellow-400">
-      component list
+      <h1>component list</h1>
     </div>
-    <div class="canvas flex-1 border-2 border-red-400">canvas</div>
+    <div class="canvas flex-1 border-2 border-red-400">
+      <h1>canvas</h1>
+      <div v-for="(comp, index) in editorStore.$state.components" :key="index">
+        {{ comp.props.text }}
+      </div>
+    </div>
     <div class="attr-panel w-60 border-2 border-green-400">attr panel</div>
   </div>
 </template>
