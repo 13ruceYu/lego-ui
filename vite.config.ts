df89533cdf4506e5828412f2b2e55963fc3dd96f
13ruceYu <bruceyuhongbo@gmail.com>
2024-01-21 07:22:12 +0800

    feat: loadEnv in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,38 @@
 /// <reference types="vitest" />
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-  // https://github.com/vitest-dev/vitest
-  test: {
-    include: ['test/**/*.test.ts'],
-    environment: 'jsdom',
-    deps: {
-      inline: ['@vue'],
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        // target: 'http://localhost:7001',
-        target: 'http://120.55.96.74:7001',
-        changeOrigin: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  const { VITE_APP_DEV_WEB_URL } = process.env
+
+  return defineConfig({
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`,
       },
     },
-  },
-  base: '/',
-})
+    // https://github.com/vitest-dev/vitest
+    test: {
+      include: ['test/**/*.test.ts'],
+      environment: 'jsdom',
+      deps: {
+        inline: ['@vue'],
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          // target: 'http://localhost:7001',
+          target: VITE_APP_DEV_WEB_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+    base: '/',
+  })
+}
